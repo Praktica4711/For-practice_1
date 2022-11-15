7fa4997d97b98dbd759c0a0b79537b89e31e7db7
Praktica4711 <113596824+Praktica4711@users.noreply.github.com>
2022-11-15 15:25:25 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class CalClass
+{
+	public static double CalClassOp(double a, double b, string oper)
+	{
+		double res = double.NaN;
+		switch (oper)
+		{
+			case "plus":
+				res = a + b;
+				break;
+			case "minus":
+				res = a - b;
+				break;
+			case "sum":
+				res = a * b;
+				break;
+			case "dif":
+				if (b != 0)
+				{
+					res = a / b;
+				}
+				break;
+			case "grade":
+                {
+					int i = 0;
+					do
+					{
+						i++;
+						res = a * a;
+					}
+					while (i <= b);
+                }
+                break;
+
+		break;
+        }
+        return res;
+	}
+	
+}
